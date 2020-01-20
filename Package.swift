4591ddabe29236589bcdbafcdd1a586c1020107b
Mohammad Porooshani <porooshani@gmail.com>
2020-01-20 12:37:21 +0330

    Added Correct SPM Support
    Signed-off-by: Mohammad Porooshani <porooshani@gmail.com>

--- Package.swift
+++ Package.swift
@@ -1,5 +1,16 @@
+// swift-tools-version:5.0
 import PackageDescription
 
 let package = Package(
-  name: "TransitionButton"
+    name: "TransitionButton",
+    products: [
+        .library(name: "TransitionButton", targets: ["TransitionButton"])
+    ],    
+    targets: [
+        .target(
+            name: "TransitionButton",
+            path: "Source"
+        )
+    ],
+    swiftLanguageVersions: [.v5]
 )
