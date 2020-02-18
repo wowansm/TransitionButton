6ba63b984bcdc9a8c94139ca64bb41dc59fa4f30
Jesús Orduño <jvoe01@me.com>
2020-02-18 13:36:23 -0700

    SPM compatibility update

--- Package.swift
+++ Package.swift
@@ -1,9 +1,3 @@
-import PackageDescription
-
-let package = Package(
-  name: "TransitionButton"
-)
-
 // swift-tools-version:5.1
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 import PackageDescription
